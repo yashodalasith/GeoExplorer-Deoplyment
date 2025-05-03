88ae207f8989c54e152a79deb387ca86b772ad52
Yashodha Jayasinghe <yashodhalasithjayasinghe@gmail.com>
2025-05-03 20:44:25 +0530

    fixed my vite config.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,6 +17,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://geo-explorer-deoplyment-71wq.vercel.app",
+        changeOrigin: true,
+        secure: true,
       },
     },
   },
